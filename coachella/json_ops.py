25939ff686ef4d26201e810e6b9864b1e3cb1656
LuisJackRamirez <l_armando_2000@hotmail.com>
2022-05-25 13:10:37 -0500

    Added parameter 'username' to method 'create_json ()', which will allow to filter data by user

--- coachella/json_ops.py
+++ coachella/json_ops.py
@@ -1,24 +1,29 @@
 import json
 
+# Uncomment following lines if testing from server.py
+# from horario import get_horario
+# from kardex import get_kardex
+# from career import get_career
+
 from coachella.horario import get_horario
 from coachella.kardex import get_kardex
 from coachella.career import get_career
 
-def create_json (query):
+def create_json (query, username):
     # Creates JSON file
     if query == 1:
         # Horario
-        horario_json = json.dumps (get_horario ())
+        horario_json = json.dumps (get_horario (username))
 
         return horario_json
     elif query == 2:
         # Kardex
-        kardex_json = json.dumps (get_kardex ())
+        kardex_json = json.dumps (get_kardex (username))
 
         return kardex_json
     elif query == 3:
         # Créditos
-        creditos_json = json.dumps (get_career ())
+        creditos_json = json.dumps (get_career (username))
 
         return creditos_json
 
