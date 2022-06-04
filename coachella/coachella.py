a45ef1f9dbebeab4ed62258ed3f374c154cb8fac
LuisJackRamirez <l_armando_2000@hotmail.com>
2022-06-04 15:22:46 -0500

    'coachella.py' now uses a dequeue to store queries, which will be shown in the adming page.

--- coachella/coachella.py
+++ coachella/coachella.py
@@ -1,9 +1,13 @@
 from coachella.json_ops import create_json
 from coachella.nlp import read_query
 
+from collections import deque
+
 from flask import Blueprint
 from flask import render_template
 from flask import request
+
+import datetime
 
 # Blueprints allow us to organize related views 
 # and other code. We register the views in the
@@ -19,10 +23,13 @@ from flask import request
 bp = Blueprint('coachella', __name__, url_prefix='/coachella')
 
 # Here we will hold all requests
-cumulative = ['History']
+q = deque ()
+
+for i in range(10):
+    q.append ('')
 
 # When user visits the "coachella/" URL, the
-# 'handle_equest' view will return HTML with
+# 'handle_request' view will return HTML with
 # the voice requests history.
 # 
 # For now, it won't require a login
@@ -36,7 +43,11 @@ def handle_request ():
         print (request.form)
 
         # Obtain query to show on server's website
-        cumulative.append (str (value))
+        q.popleft ()
+        now = datetime.datetime.now ()
+        q.append ("[" + str(now.year) + "-" + f'{now.month:02}' + "-" + f'{now.day:02}' + " - "
+            + f'{now.hour:02}' + ":" + f'{now.minute:02}' + ":" + f'{now.second:02}' + " - " + request.remote_addr + "] -- "
+            + str(username) + ": " + value)
 
         # Query will be analyzed.
         user_request = read_query (value)
@@ -63,5 +74,5 @@ def handle_request ():
     # Handles website history
     elif request.method == 'GET':
         # print ("Texto obtenido: ", str (cumulative[-1]))
-        return render_template ("index.html", msg = str(cumulative[0]))
+        return render_template ("index.html", msg = reversed(q))
         # return render_template ("index.html", msg = "\n".join (cumulative))
