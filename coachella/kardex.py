be88356fb55e6e930f0030be0bcdc48d06c612c9
LuisJackRamirez <l_armando_2000@hotmail.com>
2022-06-06 19:59:55 -0500

    Kardex and Career operations are supported again.

--- coachella/kardex.py
+++ coachella/kardex.py
@@ -1,6 +1,8 @@
 # Kardex operations
 # eval: [ord | ext | ets]
 
+import psycopg2.extras
+
 from coachella.db import get_db
 
 # import pymysql
@@ -24,19 +26,24 @@ def asked_grades (lemma):
 def get_kardex (username):
     # Returns kardex values
     conn = get_db ()
-    conn.select_db ('saes')
+    #conn.select_db ('saes')
 
-    '''query = "SELECT materia_id,nombre,materia_cursada.grupo,calif,eval,periodo FROM materia_cursada JOIN materia ON (alumno_id = " + username + " AND id=materia_id) ORDER BY periodo;"
-    cursor = conn.cursor (pymysql.cursors.DictCursor)
+    #query = "SELECT materia_id,nombre,materia_cursada.grupo,calif,eval,periodo FROM materia_cursada JOIN materia ON (alumno_id = " + username + " AND id=materia_id) ORDER BY periodo;"
+    query = "SELECT materia_id,nombre,materia_cursada.grupo,calif,eval,periodo FROM materia_cursada JOIN materia ON (alumno_id = '" + username + "' AND id=materia_id) ORDER BY periodo;"
+    #cursor = conn.cursor (pymysql.cursors.DictCursor)
+
+    cursor = conn.cursor (
+        cursor_factory = psycopg2.extras.RealDictCursor)
+
     cursor.execute (query)
-    result = cursor.fetchall ()'''
+    result = cursor.fetchall ()
 
     kardex = {
         "json_id": 2,
         "materias": []
     }
 
-    '''for x in result:
+    for x in result:
         kardex["materias"].append (
             {
                 "id": x["materia_id"],
@@ -46,6 +53,6 @@ def get_kardex (username):
                 "eval": x["eval"],
                 "periodo": x["periodo"]
             }
-        )'''
+        )
 
     return kardex
